c6b7d1c7946392a43425e25b6a6fae05f1c612eb
shirakuubo rintaro <c0b210841a@edu.teu.ac.jp>
2022-04-19 16:18:09 +0900

    とりあえず版

---
+++ rensyu01/quiz.py
@@ -0,0 +1,20 @@
+import random
+
+def shutudai():
+    qa = random.choice(q_list)
+    print(qa["q"])
+    return qa["a"]
+
+def kaito(a):
+    kai = input("答えを入力してください")
+    if kai in a:
+        print("正解です")
+    else:
+        print("不正解")
+
+if __name__ == "__main__":
+    q_list = [{"q":"サザエさんの旦那の名前は？", "a" :["マスオ", "ますお"]},
+    {"q":"カツオの妹の名前は？", "a":["わかめ","ワカメ"]} , 
+    {"q":"タラオはカツオから見てどんな関係？", "a":["甥","おい","甥っ子","おいっこ"]}]
+    a = shutudai()
+    kaito(a)
