78a9b48f0716e6d27410ff5a0efc3bf533d12c03
shirakuubo rintaro <c0b210841a@edu.teu.ac.jp>
2022-04-26 17:27:07 +0900

    クリア機能追加

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+from tblib import Frame
+
 
 
 if __name__ == "__main__":
@@ -10,48 +12,74 @@ if __name__ == "__main__":
         num = btn["text"]
         #tkm.showinfo(txt, f"[{txt}]ボタンが押されました")
         entry.insert(tk.END, num)
+        #button["bg"] = "yellow" 色の変化
 
-    def click_equal(event):
+    def click_equal(event): 
         eqn = entry.get()
         ans = eval(eqn)
         entry.delete(0, tk.END)
         entry.insert(tk.END, ans)
+
+    def click_all_clear(event):
+        entry.delete(0, tk.END)
+        entry.insert(tk.END)
+
+    def click_clear(event):
+        pass
+
+
         
     root = tk.Tk()
     root.geometry("300x450")
+
+    root.grid_rowconfigure(0, weight=1)
 
     entry=tk.Entry(root,
                     width=10,
                     justify="right",
                     font=("Times New Roman",40))
-    entry.grid(row=0, column=0, columnspan=4)
+    entry.place(x=0, y=0)
 
     r=1
     c=1
-    for i in range (9, -1, -1):
+    for i in range (9, -1, -1): #数字の表示
         button=tk.Button(root,
                         text=i,
                         font=("Times New Roman", 30))
         button.bind("<1>", button_click)
-        button.grid(column=c, row=r, padx=10, pady=10)
+        button.grid(column=c, row=r)
         
         if i%3==1:
             r+=1
             c=0
         c+=1
     
-    button=tk.Button(root,
+    button=tk.Button(root,      #+の表示・押された時の処理
                     text="+",
                     font=("Times New Roman", 30))
     button.bind("<1>", button_click)
-    button.grid(column=2, row=4, padx=10, pady=10)
+    button.grid(column=2, row=4)
 
-    button=tk.Button(root,
+    button=tk.Button(root,      #=の表示・押された時の処理
                     text="=",
                     font=("Times New Roman", 30))
     button.bind("<1>",click_equal)
-    button.grid(row=4,column=3,padx=10,pady=10)
+    button.grid(row=4,column=3)
 
-    
+
+    #追加機能
+
+    button=tk.Button(root,      #acの表示・押された時の処理
+                    text="a",
+                    font=("Times New Roman", 30))
+    button.bind("<1>", click_all_clear)
+    button.grid(column=5, row=4)
+
+    button=tk.Button(root,      #cの表示・押された時の処理
+                    text="c",
+                    font=("Times New Roman", 30))
+    button.bind("<1>", click_clear)
+    button.grid(column=5, row=3)
+
     root.mainloop()
 
