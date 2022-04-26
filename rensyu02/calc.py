e037a2119e128844d72d71ae6fff17bf1efc9dae
shirakuubo rintaro <c0b210841a@edu.teu.ac.jp>
2022-04-26 14:55:51 +0900

    電卓

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,7 +1,18 @@
+from struct import pack
 import tkinter as tk
 import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x450")
+    root.grid()
+
+    for num in range (9, -1, -1):
+        button = tk.Button(root, text=num, font= ("Times New Roman", 30))
+        
+        c=(9-num)%3
+        r=(int)((9-num)/3)
+        button.grid(column=c, row=r, padx=10, pady=10)
+    
     root.mainloop()
+
